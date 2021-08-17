c08c1383c3f04c51a7769bff7e1b668baa28e988
Kunal <kdshah6593@gmail.com>
2021-08-17 13:15:41 -0400

    completed solution, tests passing

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,23 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+
+    attr_accessor :num
+
+    def initialize(num)
+        @num = num
+    end
+
+    def square_of_sums
+        sum = (1..self.num).reduce(:+)
+        sum**2
+    end
+
+    def sum_of_squares
+        sum = (1..self.num).map{|num| num**2}.reduce(:+)
+    end
+    
+    
+    def difference
+        square_of_sums - sum_of_squares
+    end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,14 @@
 # Implement your procedural solution here!
+def sum_square_difference(num)
+    sumOfSquares = 0
+    squareOfSums = 0
+    i = 1
+  
+    while i <= num
+      sumOfSquares += i**2
+      squareOfSums += i
+      i += 1
+    end
+  
+    difference = squareOfSums**2 - sumOfSquares
+  end
